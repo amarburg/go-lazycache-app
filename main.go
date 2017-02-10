a3fc367284e5ef648ceb70035cd6a1669d45a2b3
Aaron Marburg <amarburg@apl.washington.edu>
2017-02-10 08:55:48 -0800

    Set default port to 8080

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var OOIRawDataRootURL = "https://rawdata.oceanobservatories.org/files/"
 func main() {
 
 	var (
-		port          = flag.Int("port", 5000, "Network port to listen on (default: 5000)")
+		port          = flag.Int("port", 8080, "Network port to listen on (default: 8080)")
 		bind          = flag.String("bind", "0.0.0.0", "Network interface to bind to (defaults to 0.0.0.0)")
 		image_store   = flag.String("image-store", "", "Type of image store (none, google)")
 		google_bucket = flag.String("image-store-bucket", "", "Bucket used for Google image store")
